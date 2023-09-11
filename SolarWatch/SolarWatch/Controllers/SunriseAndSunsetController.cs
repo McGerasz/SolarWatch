a505341c77ea06ca87c9d04eb687a382035d07eb
McGerasz <mcgerasz@gmail.com>
2023-09-11 16:17:03 +0200

    Add logging for SunriseAndSunset

--- SolarWatch/SolarWatch/Controllers/SunriseAndSunsetController.cs
+++ SolarWatch/SolarWatch/Controllers/SunriseAndSunsetController.cs
@@ -12,26 +12,43 @@ namespace SolarWatch.Controllers;
 public class SunriseAndSunsetController : ControllerBase
 {
     private string APIKey = "https://api.sunrise-sunset.org/json";
+    private readonly ILogger _logger;
 
+    public SunriseAndSunsetController(ILogger<SunriseAndSunsetController> logger){
+        _logger = logger;
+    }
     [HttpGet]
     public IActionResult GetByLocation([Required]float lng, [Required]float lat)
     {
-        var Client = new WebClient();
-        var timeData = Client.DownloadString($"{APIKey}?lat={lat}&lng={lng}");
-        return Ok(ProcessJsonByLocation(timeData));
+        try
+        {
+            _logger.LogInformation("Beginning GetByLocation operation");
+            var Client = new WebClient();
+            _logger.LogInformation("Downloading data from external API");
+            var timeData = Client.DownloadString($"{APIKey}?lat={lat}&lng={lng}");
+            _logger.LogInformation("Successfully downloaded data");
+            return Ok(ProcessJsonByLocation(timeData));
+
+        }
+        catch(Exception e)
+        {
+            _logger.LogError("An error has occured while processing Your request");
+            return BadRequest();
+        }
     }
 
-    private static SunriseSunset ProcessJsonByLocation(string data)
+    private SunriseSunset ProcessJsonByLocation(string data)
     {
+        _logger.LogInformation("Processing data");
         JsonDocument json = JsonDocument.Parse(data);
         JsonElement results = json.RootElement.GetProperty("results");
-
+        
         SunriseSunset processedData = new SunriseSunset
         {
             SunriseTime = results.GetProperty("sunrise").GetString(),
             SunsetTime = results.GetProperty("sunset").GetString()
         };
-
+        _logger.LogInformation("Data processed");
         return processedData;
     }
 }

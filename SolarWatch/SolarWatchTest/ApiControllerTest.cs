c90bcb329873fbbd0a9fe14510323f59a3a491a1
McGerasz <mcgerasz@gmail.com>
2023-09-13 12:32:10 +0200

    Add first unit test

--- SolarWatch/SolarWatchTest/ApiControllerTest.cs
+++ SolarWatch/SolarWatchTest/ApiControllerTest.cs
@@ -1,6 +1,9 @@
+using dotenv.net;
 using Microsoft.Extensions.Logging;
 using Moq;
 using SolarWatch.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using SolarWatch.Model;
 
 namespace SolarWatchTest;
 
@@ -15,5 +18,19 @@ public class Tests
         _controller = new ApiController(_loggerMock.Object);
     }
 
-    
+    [Test]
+    public void GetByCityNameAndDateReturnsTheRightData()
+    {
+        //Budapest coordinates:
+        //"lat": 47.4979937
+        //"lon": 19.0403594
+        
+        //Should return:
+        //"sunrise": "4:16:41 AM"
+        //"sunset": "5:03:02 PM"
+        Environment.SetEnvironmentVariable("geolocatorApiKey", DotEnv.Read()["geolocatorApiKey"]);
+        var result = _controller.GetByCityNameAndDate("Budapest", new DateOnly(2023, 09, 13));
+        var okResult = result as OkObjectResult;
+        Assert.That(okResult.Value, Is.EqualTo(new SunriseSunset{SunriseTime = "4:16:41 AM", SunsetTime = "5:03:02 PM"}));
+    }
 }

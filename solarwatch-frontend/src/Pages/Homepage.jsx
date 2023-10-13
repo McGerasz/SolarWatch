1f060d9cb66b438dcc1cdc7944174b083ed83ebc
McGerasz <mcgerasz@gmail.com>
2023-10-13 14:21:05 +0200

    feat: add variable subtext for main page

--- solarwatch-frontend/src/Pages/Homepage.jsx
+++ solarwatch-frontend/src/Pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "../Style/Homepage.css"
+import Cookies from "js-cookie";
 
 const Layout = () => {
     return (
         <div className="Homepage">
             <div className="Homepagetext">Welcome to SolarWatch</div>
+            {Cookies.get("jwtToken") ? (<h1>Try the Sunrise and Sunset feature</h1>) : (<h1>Please login or register</h1>)}
         </div>
-    );
+            );
   };
   
   export default Layout;

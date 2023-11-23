d34a7869d561a8700d012057a7fbb53bc4919c5e
McGerasz <mcgerasz@gmail.com>
2023-11-23 13:34:49 +0100

    test: refactor integration tests' base

--- SolarWatch/SolarWatchIntegrationTests/CustomWebApplicationFactory.cs
+++ SolarWatch/SolarWatchIntegrationTests/CustomWebApplicationFactory.cs
@@ -4,27 +4,23 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using QuizWebAppIntegrationTest;
 using SolarWatch.DatabaseServices.Repositories;
 
 namespace SolarWatchIntegrationTests;
 
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
-    public Mock<ICityRepository> CityRepositoryMock { get; }
     public CityRepository CityRepository { get; }
-    public Mock<ISunriseSunsetRepository> SunriseSunsetRepositoryMock { get; }
     public SunriseSunsetRepository SunriseSunsetRepository { get; }
     public SolarWatchApiTestContext TestDbContext { get; }
     
 
     public CustomWebApplicationFactory()
     {
-        CityRepositoryMock = new Mock<ICityRepository>();
-        SunriseSunsetRepositoryMock = new Mock<ISunriseSunsetRepository>();
         TestDbContext = new SolarWatchApiTestContext();
         CityRepository = new CityRepository(TestDbContext);
         SunriseSunsetRepository = new SunriseSunsetRepository(TestDbContext);
-
     }
     
     protected override void ConfigureWebHost(IWebHostBuilder builder)
@@ -32,15 +28,13 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
         base.ConfigureWebHost(builder);
         builder.ConfigureTestServices(services =>
         {
-            services.AddSingleton(CityRepositoryMock.Object);
-
-            services.AddSingleton(SunriseSunsetRepositoryMock.Object);
-
             services.AddSingleton(CityRepository);
 
             services.AddSingleton(SunriseSunsetRepository);
 
             services.AddDbContext<SolarWatchApiTestContext>();
+            
+            services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
 
         });
 
